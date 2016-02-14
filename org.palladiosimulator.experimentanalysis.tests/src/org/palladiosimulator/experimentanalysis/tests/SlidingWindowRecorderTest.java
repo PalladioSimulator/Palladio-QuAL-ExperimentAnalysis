e9c9a67575b0d78147bb5a6dec3bd614a53aa183
ChristianStier <chrisstier@gmx.de>
2016-02-14 14:46:22 +0000

    Rolled back test plugin to current state.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/ExperimentAnalysis/trunk@30973 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.experimentanalysis.tests/src/org/palladiosimulator/experimentanalysis/tests/SlidingWindowRecorderTest.java
+++ org.palladiosimulator.experimentanalysis.tests/src/org/palladiosimulator/experimentanalysis/tests/SlidingWindowRecorderTest.java
@@ -15,8 +15,7 @@ import org.junit.Test;
 import org.palladiosimulator.experimentanalysis.ISlidingWindowListener;
 import org.palladiosimulator.experimentanalysis.SlidingWindow;
 import org.palladiosimulator.experimentanalysis.SlidingWindowRecorder;
-import org.palladiosimulator.experimentanalysis.tests.utils.StoreLastMeasurementRecorder;
-import org.palladiosimulator.experimentanalysis.windowaggregators.SlidingWindowUtilizationAggregator;
+import org.palladiosimulator.experimentanalysis.SlidingWindowUtilizationAggregator;
 import org.palladiosimulator.measurementframework.MeasuringValue;
 import org.palladiosimulator.measurementframework.TupleMeasurement;
 import org.palladiosimulator.metricspec.MetricDescription;
@@ -24,62 +23,60 @@ import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 
 public class SlidingWindowRecorderTest {
 
-    private SlidingWindowRecorder recorderUnderTest;
-    private SlidingWindow window;
-    private Measure<Double, Duration> windowLength;
-    private MeasuringValue measurement;
-    private ISlidingWindowListener windowListener;
+	private SlidingWindowRecorder recorderUnderTest;
+	private SlidingWindow window;
+	private Measure<Double, Duration> windowLength;
+	private MeasuringValue measurement;
+	private ISlidingWindowListener windowListener;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.windowLength = Measure.valueOf(10d, SI.SECOND);
+				
+		this.measurement = new TupleMeasurement(MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC_TUPLE, 
+				Measure.valueOf(3.5d, SI.SECOND), Measure.valueOf(1337L, Unit.ONE));
+		this.window = new SlidingWindowMock(this.windowLength, MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC_TUPLE, 
+				new SlidingWindowTest.DummyMoveOnStrategy());
+				
+		this.windowListener = new SlidingWindowUtilizationAggregator(new SlidingWindowAggregatorTest.StoreLastMeasurementRecorder());
+		this.recorderUnderTest = new SlidingWindowRecorder(this.window, this.windowListener);
+	}
 
-    @Before
-    public void setUp() throws Exception {
-        this.windowLength = Measure.valueOf(10d, SI.SECOND);
+	@After
+	public void tearDown() throws Exception {
+			}
 
-        this.measurement = new TupleMeasurement(MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC_TUPLE,
-                Measure.valueOf(3.5d, SI.SECOND), Measure.valueOf(1337L, Unit.ONE));
-        this.window = new SlidingWindowMock(this.windowLength,
-                MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC_TUPLE,
-                new SlidingWindowTest.DummyMoveOnStrategy());
+	@Test(expected=IllegalArgumentException.class)
+	public void testSlidingWindowRecorderCtorNoWindow() {
+		new SlidingWindowRecorder(null, this.windowListener);
+	}
 
-        this.windowListener = new SlidingWindowUtilizationAggregator(new StoreLastMeasurementRecorder());
-        this.recorderUnderTest = new SlidingWindowRecorder(this.window, this.windowListener);
-    }
+	@Test
+	public void testWriteData() {
+		assertTrue(this.window.isEmpty());
+		this.recorderUnderTest.writeData(measurement);
+		assertFalse(this.window.isEmpty());
+		assertEquals(1, this.window.getNumberOfElements());
+	}
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@Test
+	public void testFlush() {
+		this.recorderUnderTest.writeData(measurement);
+		this.recorderUnderTest.flush();
+		assertTrue(this.window.isEmpty());
+	}
+	
+	private static final class SlidingWindowMock extends SlidingWindow {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testSlidingWindowRecorderCtorNoWindow() {
-        new SlidingWindowRecorder(null, this.windowListener);
-    }
-
-    @Test
-    public void testWriteData() {
-        assertTrue(this.window.isEmpty());
-        this.recorderUnderTest.writeData(measurement);
-        assertFalse(this.window.isEmpty());
-        assertEquals(1, this.window.getNumberOfElements());
-    }
-
-    @Test
-    public void testFlush() {
-        this.recorderUnderTest.writeData(measurement);
-        this.recorderUnderTest.flush();
-        assertTrue(this.window.isEmpty());
-    }
-
-    private static final class SlidingWindowMock extends SlidingWindow {
-
-        public SlidingWindowMock(Measure<Double, Duration> windowLength, MetricDescription acceptedMetrics,
-                ISlidingWindowMoveOnStrategy moveOnStrategy) {
+	    public SlidingWindowMock(Measure<Double, Duration> windowLength, MetricDescription acceptedMetrics, ISlidingWindowMoveOnStrategy moveOnStrategy) {
             super(windowLength, acceptedMetrics, moveOnStrategy);
             // TODO Auto-generated constructor stub
         }
-
+	    
         public SlidingWindowMock(Measure<Double, Duration> windowLength, Measure<Double, Duration> increment,
                 MetricDescription acceptedMetrics, ISlidingWindowMoveOnStrategy moveOnStrategy) {
             super(windowLength, increment, acceptedMetrics, moveOnStrategy);
             // TODO Auto-generated constructor stub
         }
-    }
+	}
 }
