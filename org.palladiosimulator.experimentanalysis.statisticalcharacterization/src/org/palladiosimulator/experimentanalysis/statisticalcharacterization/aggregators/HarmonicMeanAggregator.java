21587d5a0fa3ed75249c4cd523298bcd52e6cb49
florian-rosenthal <florian.rosenthal@kit.edu>
2016-02-26 16:11:18 +0000

    change implementation of harmonic mean
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/ExperimentAnalysis/branches/extendedMonitorRepository@31238 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.experimentanalysis.statisticalcharacterization/src/org/palladiosimulator/experimentanalysis/statisticalcharacterization/aggregators/HarmonicMeanAggregator.java
+++ org.palladiosimulator.experimentanalysis.statisticalcharacterization/src/org/palladiosimulator/experimentanalysis/statisticalcharacterization/aggregators/HarmonicMeanAggregator.java
@@ -1,7 +1,6 @@
 package org.palladiosimulator.experimentanalysis.statisticalcharacterization.aggregators;
 
 import java.util.Collection;
-import java.util.DoubleSummaryStatistics;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -14,40 +13,42 @@ import org.palladiosimulator.metricspec.NumericalBaseMetricDescription;
 import org.palladiosimulator.recorderframework.IRecorder;
 
 /**
- * {@link SlidingWindowAggregator} which computes the geometric mean from the measurements in the
- * window once it moves on:<br>
- * It is equal to the reciprocal of the arithmetic mean of the reciprocals of the measurements.<br>
- * Note that it is not defined if any of the measurements is exactly 0. In such a case this
- * implementation returns 0
+ * {@link SlidingWindowAggregator} which computes the geometric mean from the
+ * measurements in the window once it moves on:<br>
+ * It is equal to the reciprocal of the arithmetic mean of the reciprocals of
+ * the measurements.<br>
+ * Note that it is not defined if any of the measurements is exactly 0. In such
+ * a case this implementation returns 0
  * 
  * @author Florian Rosenthal
  *
  */
 public class HarmonicMeanAggregator extends StatisticalCharacterizationAggregator {
 
-    public HarmonicMeanAggregator(NumericalBaseMetricDescription expectedWindowMetric) {
-        super(expectedWindowMetric);
-    }
+	public HarmonicMeanAggregator(NumericalBaseMetricDescription expectedWindowMetric) {
+		super(expectedWindowMetric);
+	}
 
-    public HarmonicMeanAggregator(IRecorder recorderToWriteInto, NumericalBaseMetricDescription expectedWindowMetric) {
-        super(recorderToWriteInto, expectedWindowMetric);
-    }
+	public HarmonicMeanAggregator(IRecorder recorderToWriteInto, NumericalBaseMetricDescription expectedWindowMetric) {
+		super(recorderToWriteInto, expectedWindowMetric);
+	}
 
-    public HarmonicMeanAggregator(Collection<IRecorder> recordersToWriteInto,
-            NumericalBaseMetricDescription expectedWindowMetric) {
-        super(recordersToWriteInto, expectedWindowMetric);
-    }
+	public HarmonicMeanAggregator(Collection<IRecorder> recordersToWriteInto,
+			NumericalBaseMetricDescription expectedWindowMetric) {
+		super(recordersToWriteInto, expectedWindowMetric);
+	}
 
-    @Override
-    protected Measure<Double, Quantity> calculateStatisticalCharaterization(Iterable<MeasuringValue> windowData) {
-        // harmonic mean is not defined in case that any of the elements equals zero
-        // this implementation will then return 0
-        DoubleSummaryStatistics inverseValuesStatistics = StreamSupport.stream(windowData.spliterator(), false)
-                .map(measuringValue -> measuringValue.getMeasureForMetric(this.dataMetric))
-                .collect(Collectors.summarizingDouble(measure -> 1d / measure.doubleValue(this.dataDefaultUnit)));
-        long numberOfElements = inverseValuesStatistics.getCount();
-        return Measure.valueOf(numberOfElements == 0 || inverseValuesStatistics.getSum() == Double.NaN ? 0
-                : numberOfElements / inverseValuesStatistics.getSum(), this.dataDefaultUnit);
-    }
+	@Override
+	protected Measure<Double, Quantity> calculateStatisticalCharaterization(Iterable<MeasuringValue> windowData) {
+		// harmonic mean is not defined in case that any of the elements equals
+		// zero, this implementation will then return 0
+		double arithmeticMeanOfInverses = StreamSupport.stream(windowData.spliterator(), false)
+				.map(measuringValue -> measuringValue.getMeasureForMetric(this.dataMetric))
+				.collect(Collectors.averagingDouble(measure -> 1d / measure.doubleValue(this.dataDefaultUnit)));
+
+		return Measure.valueOf(arithmeticMeanOfInverses == Double.NaN || arithmeticMeanOfInverses == 0 ? 0
+				: 1 / arithmeticMeanOfInverses, this.dataDefaultUnit);
+
+	}
 
 }
