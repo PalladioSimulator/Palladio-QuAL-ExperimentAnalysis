00b67f7c7d2be7390c5fe6985405e9ce2acea5d4
florian-rosenthal <florian.rosenthal@kit.edu>
2016-04-28 13:58:20 +0000

    incorporate test cases for new utilization filter functionality
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/ExperimentAnalysis/trunk@31895 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.experimentanalysis.utilizationfilter.tests/src/org/palladiosimulator/experimentanalysis/utilizationfilter/tests/UtilizationFilterTest.java
+++ org.palladiosimulator.experimentanalysis.utilizationfilter.tests/src/org/palladiosimulator/experimentanalysis/utilizationfilter/tests/UtilizationFilterTest.java
@@ -1,6 +1,7 @@
 package org.palladiosimulator.experimentanalysis.utilizationfilter.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -35,172 +36,185 @@ import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 
 public class UtilizationFilterTest {
 
-	private UtilizationFilter filterUnderTest;
-	private IDataSource inputData;
-	private MeasuringValue expectedUtilization;
-	private PropertyConfigurable filterUnderTestProperties;
-	private Measure<Double, Duration> defaultWindowLength;
-	private static final MetricSetDescription expectedInputDataMetric = MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC_TUPLE;
-	private static final MetricSetDescription expectedOutputDataMetric = MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE_TUPLE;
-	//constant that denotes  the maximum delta between double values for which both numbers are still considered equal
-	private static final double DELTA = Math.pow(10, -12);
+    private UtilizationFilter filterUnderTest;
+    private IDataSource inputData;
+    private MeasuringValue expectedUtilization;
+    private PropertyConfigurable filterUnderTestProperties;
+    private Measure<Double, Duration> defaultWindowLength;
+    private static final MetricSetDescription expectedInputDataMetric = MetricDescriptionConstants.STATE_OF_ACTIVE_RESOURCE_METRIC_TUPLE;
+    private static final MetricSetDescription expectedOutputDataMetric = MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE_TUPLE;
+    // constant that denotes the maximum delta between double values for which both numbers are
+    // still considered equal
+    private static final double DELTA = Math.pow(10, -12);
 
-	@SuppressWarnings("unchecked")
-	@Before
-	public void setUp() throws Exception {
-		this.filterUnderTest = new UtilizationFilter();
-		this.filterUnderTestProperties = this.filterUnderTest
-				.createProperties();
-		// the following cast is typesafe, as property is of required measure
-		// type (cf. class UtilizationFilterConfiguration)
-		this.defaultWindowLength = (Measure<Double, Duration>) this.filterUnderTestProperties
-				.getDefaultConfiguration().get(UtilizationFilterConfiguration.WINDOW_LENGTH_KEY);
-		this.inputData = new MockDataSource(expectedInputDataMetric,
-				this.defaultWindowLength);
-		this.expectedUtilization = new TupleMeasurement(
-				expectedOutputDataMetric, this.defaultWindowLength,
-				Measure.valueOf(0.7, Unit.ONE));
-	}
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setUp() throws Exception {
+        this.filterUnderTest = new UtilizationFilter();
+        this.filterUnderTestProperties = this.filterUnderTest.createProperties();
+        // the following cast is typesafe, as property is of required measure
+        // type (cf. class UtilizationFilterConfiguration)
+        this.defaultWindowLength = (Measure<Double, Duration>) this.filterUnderTestProperties.getDefaultConfiguration()
+                .get(UtilizationFilterConfiguration.WINDOW_LENGTH_KEY);
+        this.inputData = new MockDataSource(expectedInputDataMetric, this.defaultWindowLength);
+        this.expectedUtilization = new TupleMeasurement(expectedOutputDataMetric, this.defaultWindowLength,
+                Measure.valueOf(0.7, Unit.ONE));
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test(expected = IllegalStateException.class)
-	public void testGetDataStreamNoDatasource() {
-		this.filterUnderTest.getDataStream();
-	}
+    @Test
+    public void testCanAccept() {
+        IDataSource correctSource = this.inputData;
+        assertTrue(this.filterUnderTest.canAccept(correctSource));
 
-	@Test
-	public void testGetDataStream() {
-		// make sure that input data is set, then obtain result stream
-		this.filterUnderTest.setDataSource(inputData);
-		IDataStream<MeasuringValue> result = this.filterUnderTest.getDataStream();
-		// now check whether the result stream is as expected: one utilization
-		// measure is expected
-		assertTrue(result.isCompatibleWith(expectedOutputDataMetric));
-		assertEquals(expectedOutputDataMetric, result.getMetricDesciption());
-		assertEquals(1, result.size());
-		
-		MeasuringValue measurement = result.iterator().next();
-		assertTrue(measurement.isCompatibleWith(expectedOutputDataMetric));
-		Measure<Double, Dimensionless> expected = this.expectedUtilization.getMeasureForMetric(MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE);
-		Measure<Double, Dimensionless> actual = measurement.getMeasureForMetric(MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE);
-		assertEquals(expected.getValue(), actual.getValue(), DELTA);
+        // the utilization tuple metric is also a valid input metric ('overall utilization')
+        correctSource = new MockDataSource(expectedOutputDataMetric, this.defaultWindowLength);
+        assertTrue(this.filterUnderTest.canAccept(correctSource));
+    }
 
-		// close the stream and check length afterwards
-		result.close();
-		assertEquals(0, result.size());
-	}
+    @Test
+    public void testCanAcceptWrongMetric() {
+        IDataSource wrongSource = new MockDataSource(MetricDescriptionConstants.AGGREGATED_COST_OVER_TIME,
+                this.defaultWindowLength);
+        assertFalse(this.filterUnderTest.canAccept(wrongSource));
+    }
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testUtilizationFilterCtorDatasourceWrongMetric() {
-		IDataSource dataSource = new MockDataSource(MetricDescriptionConstants.EXECUTION_RESULT_METRIC_TUPLE, this.defaultWindowLength);
-		new UtilizationFilter(dataSource);
-	}
+    @Test(expected = NullPointerException.class)
+    public void testGetDataStreamNoDatasource() {
+        this.filterUnderTest.getDataStream();
+    }
 
-	private static class MockDataSource extends AbstractDataSource {
+    @Test
+    public void testGetDataStream() {
+        // make sure that input data is set, then obtain result stream
+        this.filterUnderTest.setDataSource(inputData);
+        IDataStream<MeasuringValue> result = this.filterUnderTest.getDataStream();
+        // now check whether the result stream is as expected: one utilization
+        // measure is expected
+        assertTrue(result.isCompatibleWith(expectedOutputDataMetric));
+        assertEquals(expectedOutputDataMetric, result.getMetricDesciption());
+        assertEquals(1, result.size());
 
-		private List<MeasuringValue> data = new ArrayList<>();
-		private Measure<Double, Duration> windowLength;
+        MeasuringValue measurement = result.iterator().next();
+        assertTrue(measurement.isCompatibleWith(expectedOutputDataMetric));
+        Measure<Double, Dimensionless> expected = this.expectedUtilization
+                .getMeasureForMetric(MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE);
+        Measure<Double, Dimensionless> actual = measurement
+                .getMeasureForMetric(MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE);
+        assertEquals(expected.getValue(), actual.getValue(), DELTA);
 
-		private MockDataSource(MetricDescription metric,
-				Measure<Double, Duration> windowLength) {
-			super(metric);
-			this.windowLength = windowLength;
-			initializeDataSource();
-		}
+        // close the stream and check length afterwards
+        result.close();
+        assertEquals(0, result.size());
+    }
 
-		private void initializeDataSource() {
-			// create the filter input data, (point in time, state of active
-			// resource) tuples
-			Measure<Long, Dimensionless> idleStateMeasure = Measure.valueOf(0L, Unit.ONE);
-			Measure<Long, Dimensionless> busyStateMeasure = Measure.valueOf(42L, Unit.ONE);
+    @Test(expected = IllegalArgumentException.class)
+    public void testUtilizationFilterCtorDatasourceWrongMetric() {
+        IDataSource dataSource = new MockDataSource(MetricDescriptionConstants.EXECUTION_RESULT_METRIC_TUPLE,
+                this.defaultWindowLength);
+        new UtilizationFilter(dataSource);
+    }
 
-			// start with utilization
-			Measure<Double, Duration> pointInTimeMeasure = Measure.valueOf(0d, SI.SECOND);
-			this.data.add(new TupleMeasurement(expectedInputDataMetric,
-					pointInTimeMeasure, busyStateMeasure));
+    private static class MockDataSource extends AbstractDataSource {
 
-			// no utilization next
-			pointInTimeMeasure = Measure.valueOf(
-					this.windowLength.doubleValue(SI.MILLI(SI.SECOND)) * 0.3,
-					SI.MILLI(SI.SECOND));
-			this.data.add(new TupleMeasurement(expectedInputDataMetric,
-					pointInTimeMeasure, idleStateMeasure));
+        private final List<MeasuringValue> data = new ArrayList<>();
+        private final Measure<Double, Duration> windowLength;
 
-			// again some processes active
-			pointInTimeMeasure = Measure.valueOf(
-					this.windowLength.doubleValue(SI.CENTI(SI.SECOND)) * 0.6,
-					SI.CENTI(SI.SECOND));
-			this.data.add(new TupleMeasurement(expectedInputDataMetric,
-					pointInTimeMeasure, busyStateMeasure));
+        private MockDataSource(MetricDescription metric, Measure<Double, Duration> windowLength) {
+            super(metric);
+            this.windowLength = windowLength;
+            initializeDataSource();
+        }
 
-			// this setup should yield a utilization of 0.70 (70%)
-			// 70% of window is activity, rest is idleness
-		}
+        private void initializeDataSource() {
+            // create the filter input data, (point in time, state of active
+            // resource) tuples
+            Measure<Long, Dimensionless> idleStateMeasure = Measure.valueOf(0L, Unit.ONE);
+            Measure<Long, Dimensionless> busyStateMeasure = Measure.valueOf(42L, Unit.ONE);
 
-		//this gets the input data, i.e., (point in time, state of active resource) tuples
-		@Override
-		public <M extends IMeasureProvider> IDataStream<M> getDataStream() {
-			return new IDataStream<M>() {
+            // start with utilization
+            Measure<Double, Duration> pointInTimeMeasure = Measure.valueOf(0d, SI.SECOND);
+            this.data.add(new TupleMeasurement(expectedInputDataMetric, pointInTimeMeasure, busyStateMeasure));
 
-				@Override
-				public Iterator<M> iterator() {
-					@SuppressWarnings("unchecked")
-					//that cast is type safe as Measurement implements the IMeasureProvider interface 
-					Iterator<M> result = (Iterator<M>) MockDataSource.this.data.iterator();
-					return result;
-				}
+            // no utilization next
+            pointInTimeMeasure = Measure.valueOf(this.windowLength.doubleValue(SI.MILLI(SI.SECOND)) * 0.3,
+                    SI.MILLI(SI.SECOND));
+            this.data.add(new TupleMeasurement(expectedInputDataMetric, pointInTimeMeasure, idleStateMeasure));
 
-				@Override
-				public MetricDescription getMetricDesciption() {
-					return expectedInputDataMetric;
-				}
+            // again some processes active
+            pointInTimeMeasure = Measure.valueOf(this.windowLength.doubleValue(SI.CENTI(SI.SECOND)) * 0.6,
+                    SI.CENTI(SI.SECOND));
+            this.data.add(new TupleMeasurement(expectedInputDataMetric, pointInTimeMeasure, busyStateMeasure));
 
-				@Override
-				public boolean isCompatibleWith(MetricDescription other) {
-					return getMetricDesciption().equals(other);
-				}
+            // this setup should yield a utilization of 0.70 (70%)
+            // 70% of window is activity, rest is idleness
+        }
 
-				@Override
-				public void close() {
-					MockDataSource.this.data.clear();
-				}
+        // this gets the input data, i.e., (point in time, state of active resource) tuples
+        @Override
+        public <M extends IMeasureProvider> IDataStream<M> getDataStream() {
+            return new IDataStream<M>() {
 
-				@Override
-				public int size() {
-					return MockDataSource.this.data.size();
-				}
-			};
-		}
+                @Override
+                public Iterator<M> iterator() {
+                    @SuppressWarnings("unchecked")
+                    // that cast is type safe as Measurement implements the IMeasureProvider
+                    // interface
+                    Iterator<M> result = (Iterator<M>) MockDataSource.this.data.iterator();
+                    return result;
+                }
 
-		@Override
-		protected PropertyConfigurable createProperties() {
-			return new PropertyConfigurable() {
+                @Override
+                public MetricDescription getMetricDesciption() {
+                    return expectedInputDataMetric;
+                }
 
-				@Override
-				public Class<?> getPropertyType(String key) {
-					return null;
-				}
+                @Override
+                public boolean isCompatibleWith(MetricDescription other) {
+                    return getMetricDesciption().equals(other);
+                }
 
-				@Override
-				public Set<String> getKeys() {
-					return Collections.emptySet();
-				}
+                @Override
+                public void close() {
+                    MockDataSource.this.data.clear();
+                }
 
-				@Override
-				public Map<String, Object> getDefaultConfiguration() {
-					return Collections.emptyMap();
-				}
-			};
-		}
+                @Override
+                public int size() {
+                    return MockDataSource.this.data.size();
+                }
+            };
+        }
 
-		@Override
-		public MeasuringPoint getMeasuringPoint() {
-			return null;
-		}
+        @Override
+        protected PropertyConfigurable createProperties() {
+            return new PropertyConfigurable() {
 
-	}
+                @Override
+                public Class<?> getPropertyType(String key) {
+                    return null;
+                }
+
+                @Override
+                public Set<String> getKeys() {
+                    return Collections.emptySet();
+                }
+
+                @Override
+                public Map<String, Object> getDefaultConfiguration() {
+                    return Collections.emptyMap();
+                }
+            };
+        }
+
+        @Override
+        public MeasuringPoint getMeasuringPoint() {
+            return null;
+        }
+
+    }
 
 }
